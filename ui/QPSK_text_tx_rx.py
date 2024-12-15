15c2be9a2fe895723d22942a05b10ca5f04fa44e
yhnx <yehenasuramuni@hotmail.com>
2024-12-16 01:50:47 +0530

    reciever.py made

--- ui/QPSK_text_tx_rx.py
+++ ui/QPSK_text_tx_rx.py
@@ -31,6 +31,21 @@ import os
 
 input_file = os.environ['INPUT_FILE'] # Define input file path
 output_file = os.environ['OUTPUT_FILE']  # Define output file path
+tmp_file = "/input.tmp"  # Temporary file path
+
+def add_preamble():
+        # Example binary string
+    binarypreamble = b'11000110101100111111010110101000011010110011111000110101100'
+    file_path =input_file
+    with open(file_path, 'rb') as file:
+        plaintext = file.read()
+    preamble = binarypreamble * 300
+    detect_sequence = b'sts'  # Sequence to detect preamble
+    
+    with open(tmp_file, 'wb') as output:
+        output.write(preamble + detect_sequence + plaintext + detect_sequence + preamble)
+
+
 
 
 class QPSK_text_tx_rx(gr.top_block, Qt.QWidget):
@@ -511,7 +526,7 @@ class QPSK_text_tx_rx(gr.top_block, Qt.QWidget):
         self.blocks_repack_bits_bb_0_0_0 = blocks.repack_bits_bb(1, 8, 'packet_len', False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_0_0 = blocks.repack_bits_bb(8, 1, 'packet_len', False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, 8, '', True, gr.GR_MSB_FIRST)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, input_file, False, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, tmp_file, False, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
         self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, output_file, False)
         self.blocks_file_sink_0.set_unbuffered(False)
