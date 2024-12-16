82378b3e915aedd38e5ad698816d2a2238d2453b
praveen <himasha2k2@gmail.com>
2024-12-16 09:30:25 +0530

    update

--- ui/Telelink.py
+++ ui/Telelink.py
@@ -32,6 +32,68 @@ from gnuradio import soapy
 import sip
 
 
+def snipfcn_snippet_0(self):
+    # Function to remove both front and back preambles and sequence from file
+    import time
+    import subprocess
+    def remove_preamble(file_path):
+        global content
+
+        detect_sequence = b'sts'
+
+        #with open(file_path, 'rb') as file:
+            #content = file.read()
+
+        start_index = content.find(detect_sequence)
+        if start_index != -1:
+            content = content[start_index + len(detect_sequence):]
+
+        end_index = content.rfind(detect_sequence)
+        if end_index != -1:
+            content = content[:end_index]
+
+    def tag():
+         global content
+         detected=False
+         typs=[b'mp3',b'jpeg',b'mp4']
+
+         while(not(detected)):
+            with open('./rx.tmp', 'rb') as file:
+                content = file.read()
+            time.sleep(1)
+            for tag in typs:
+                position= content.rfind(tag)
+                if position != -1:
+                    remove_preamble('./rx.tmp')
+                    if(tag==b'jpeg'):
+                        with open("./out.jpg", 'wb') as file:
+                            file.write(content)
+                            detected=True
+                            subprocess.run(["open","./out.jpg"])
+                    if(tag==b'mp3'):
+                        with open("./out.mp3", 'wb') as file:
+                            file.write(content)
+                            subprocess.run(["open","./out.mp3"])
+                            detected=True
+                    if(tag==b'ts'):
+                        with open("./out.ts", 'wb') as file:
+                            file.write(content)
+                            subprocess.run(["open","./out.ts"])
+                            detected=True
+                    if(tag==b'mp4'):
+                        with open("./out.mp4", 'wb') as file:
+                            file.write(content)
+                            subprocess.run(["open","./out.mp4"])
+                            detected=True
+                        print("detected")
+
+
+    # Remove both front and back preambles and sequence from the output.tmp file
+    tag()
+
+
+def snippets_main_after_stop(tb):
+    snipfcn_snippet_0(tb)
 
 class Telelink(gr.top_block, Qt.QWidget):
 
@@ -569,7 +631,7 @@ class Telelink(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
-
+        snippets_main_after_stop(self)
         event.accept()
 
     def get_MTU(self):
@@ -821,7 +883,7 @@ def main(top_block_cls=Telelink, options=None):
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
-
+        snippets_main_after_stop(tb)
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
